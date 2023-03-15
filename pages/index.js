609f7adab6814bd3ba47ec2ed660e16821c29391
David Rhodes <david@espressocode.tech>
2023-03-15 14:53:45 -0500

    removes old component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
-// import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import formatDate from '@/lib/utils/formatDate'
 import { GraphQLClient, gql } from 'graphql-request'
